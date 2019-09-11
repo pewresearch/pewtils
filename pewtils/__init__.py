46c96a18c6fa428d33098cbf163428bcf096ca7b
pvankessel <pvankessel@pewresearch.org>
2019-09-11 16:45:15 -0400

    Move function from django_twitter

--- pewtils/__init__.py
+++ pewtils/__init__.py
@@ -327,3 +327,41 @@ class classproperty(object):
 
     def __get__(self, owner_self, owner_cls):
         return self.fget(owner_cls)
+
+
+def scan_dictionary(search_dict, field):
+
+    """
+    Takes a dict with nested lists and dicts,
+    and searches all dicts for a key of the field
+    provided.
+    """
+
+    fields_found = []
+    key_path = []
+
+    for key, value in search_dict.items():
+        if key == field:
+            fields_found.append(value)
+            new_str = str(key) + "/"
+            key_path.append(new_str)
+
+        elif isinstance(value, dict):
+            results, path = scan_dictionary(value, field)
+            for result in results:
+                fields_found.append(result)
+            for road in path:
+                new_str = str(key) + "/" + road
+                key_path.append(new_str)
+
+        elif isinstance(value, list):
+            for item in value:
+                if isinstance(item, dict):
+                    more_results, more_path = scan_dictionary(item, field)
+                    for another_result in more_results:
+                        fields_found.append(another_result)
+                    for another_road in more_path:
+                        new_str = str(key) + "/" + another_road
+                        key_path.append(new_str)
+
+    return fields_found, key_path
