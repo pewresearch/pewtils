133febc10756e58ba1fb925593490da588b8e69e
pvankessel <pvankessel@pewresearch.org>
2019-09-20 16:20:03 -0400

    (still in progress) improved canonical_link; expanded list of URL shorteners; moved canonical_link from internal to main HTTP module

--- pewtils/http.py
+++ pewtils/http.py
@@ -1,12 +1,14 @@
 from __future__ import print_function
 from __future__ import division
 from builtins import str
-import re, time, threading
+import re, time, threading, requests
+from six.moves.urllib import parse as urlparse
 
 from functools import wraps
 from random import uniform
 from bs4 import BeautifulSoup
 from unidecode import unidecode
+from tld import get_tld
 
 from pewtils import get_hash, decode_text
 
@@ -35,10 +37,6 @@ def new_random_number(attempt = 1, base_interval = 1.0, max_sleep = 10):
     '''
 
     return uniform(0, min(max_sleep, base_interval * 2 ** attempt))
-
-
-
-
 
 
 def strip_html(html, simple=False, break_tags=None):
@@ -100,68 +98,156 @@ def strip_html(html, simple=False, break_tags=None):
                 re.sub(r"\<\/?div\>|\<\/?p\>|\<br\>", "\n", html).split("\n")
         ])
 
+GENERAL_LINK_SHORTENERS = [
+    "wp.me",
+    "trib.al",
+    "t.co",
+    "spr.ly",
+    "shout.lt",
+    "shar.es",
+    "scr.bi",
+    "ow.ly",
+    "ora.cl",
+    "is.gd",
+    "ht.ly",
+    "goo.gl",
+    "fw.to",
+    "dlvr.it",
+    "cutt.ly",
+    "buff.ly",
+    "j.mp",
+    "hubs.ly",
+    "bit.ly",
+    "hub.am",
+    "ht.ly",
+    "every.tw",
+    "crwd.fr",
+    "bit.do",
+    "snp.tv",
+    "snip.ly",
+    "shr.gs",
+    "qoo.ly",
+    "po.st",
+    "fwdaga.in",
+    "fw.to",
+    "tiny.cc",
+    "owl.li",
+    "mvnt.us",
+    "msgp.pl",
+    "flip.it",
+    "urlm.in",
+    "trib.it",
+    "shoo.ly",
+    "loom.ly",
+    "disq.us",
+    "abre.ai",
+    "bit.do",
+    "adf.ly",
+    "su.pr"
+]
 
-LINK_SHORTENER_MAP = {
-    "wapo.st": "washingtonpost.com",
+# TODO: perhaps separate out active and inactive URL shorteners
+VANITY_LINK_SHORTENERS = {
+    "pdora.co": "pandora.com",
+    "tgam.ca": "theglobeandmail.com",
+    "stjr.nl": "statesmanjournal.com",
+    "amzn.com": "amazon.com",
+    "meetu.ps": "meetup.com",
+    "flic.kr": "flickr.com",
+    "ampr.gs": "americanprogress.org",
+    "rol.st": "rollingstone.com",
+    "mapq.st": "mapquest.com",
+    "nws.mx": "newsmax.com",
+    "newspr.es": "news-press.com",
+    "interc.pt": "theintercept.com",
+    "ind.pn": "independent.co.uk",
+    "bizj.us": "bizjournals.com",
+    "pewrsr.ch": "pewresearch.org",
+    "on.fb.me": "facebook.com",
+    "fb.me": "facebook.com",
+    "dbtg.tv": "bundestag.de",
+    "m.me": "messenger.com",
+    "trib.in": "chicagotribune.com",
+    # "tonyr.co": "tonyrobbins.com",
+    # "d-news.co": "dallasnews.com",
+    "wtrne.ws": "timesrecordnews.com",
+    "icont.ac": "icontact-archive.com",
+    # "wapo.st": "washingtonpost.com",
+    "entm.ag": "entrepreneur.com",
+    "engt.co": "engadget.com",
     "nyti.ms": "nytimes.com",
+    "gph.is": "giphy.com",
     "fxn.ws": "foxnews.com",
+    "conta.cc": "constantcontact.com",
     "wh.gov": "whitehouse.gov",
-    "goo.gl": "google.com",
-    "huff.to": "huffingtonpost.com",
+    # "hrld.us": "miamiherald.com",
+    "harlem.in": "harlemunited.org",
     "n.pr": "npr.org",
     "apne.ws": "apnews.com",
     "ap.org": "apnews.com",
     "youtu.be": "youtube.com",
+    "grnol.co": "greenvilleonline.com",
     "washex.am": "washingtonexaminer.com",
-    "wpo.st": "washingtonpost.com",
+    # "wpo.st": "washingtonpost.com",
     "usat.ly": "usatoday.com",
     "cbsloc.al": "cbslocal.com",
     "cbsn.ws": "cbsnews.com",
     "bloom.bg": "bloomberg.com",
-    "abcn.ws": "abcnews.com",
+    "wtr.ie": "water.ie",
+    # "abcn.ws": "abcnews.com",
     "reut.rs": "reuters.com",
     "ti.me": "time.com",
     "politi.co": "politico.com",
     "cnn.it": "cnn.com",
-    "cs.pn": "c-span.org",
-    "t.co": "twitter.com",
+    # "cs.pn": "c-span.org",
     "bzfd.it": "buzzfeed.com",
+    "bcove.me": "brightcove.com",
     "c-spanvideo.org": "c-span.org",
     "lat.ms": "latimes.com",
     "nbcnews.to": "nbcnews.com",
+    "ab.co": "abc.net.au",
     "onforb.es": "forbes.com",
+    "nwsdy.li": "newsday.com",
+    "spon.de": "spiegel.de",
+    "mol.im": "dailymail.co.uk",
     "theatln.tc": "theatlantic.com",
     "yhoo.it": "yahoo.com",
+    "jrnl.ie": "thejournal.ie",
     "sen.gov": "senate.gov",
     "es.pn": "espn.com",
-    "dot.gov": "transportation.gov",
+    # "dot.gov": "transportation.gov",
     "wrd.cm": "wired.com",
     "cour.at": "courant.com",
     "read.bi": "businessinsider.com",
     "dailysign.al": "dailysignal.com",
+    "buswk.co": "bloomberg.com",
     "cnnmon.ie": "cnn.com",
     "cnb.cx": "cnbc.com",
     "atxne.ws": "statesman.com",
     "slate.me": "slate.com",
     "detne.ws": "detroitnews.com",
-    "huffpost.com": "huffingtonpost.com",
     "injo.com": "ijr.com",
     "fb.me": "facebook.com",
+    "njersy.co": "northjersey.com",
+    "nj-ne.ws": "nj.com",
     "amzn.to": "amazon.com",
     "fb.com": "facebook.com",
     "lnkd.in": "linkedin.com",
+    "linkd.in": "linkedin.com",
+    "histv.co": "history.com",
+    "gph.is": "giphy.com",
     "mailchi.mp": "mailchimp.com",
-    "hrld.us": "miamiherald.com",
-    "oh.us": "state.oh.us",
-    "ofa.bo": "ofa.us",
+    # "hrld.us": "miamiherald.com",
+    # "oh.us": "state.oh.us",
+    # "ofa.bo": "ofa.us",
     "indy.st": "indystar.com",
     "strib.mn": "startribune.com",
     "nydn.us": "nydailynews.com",
     "bsun.md": "baltimoresun.com",
     "bayareane.ws": "eastbaytimes.com",
-    "pa.us": "state.pa.us",
-    "g.co": "google.com",
-    "mn.us": "state.mn.us",
+    # "pa.us": "state.pa.us",
+    # "g.co": "google.com",
+    # "mn.us": "state.mn.us",
     "thebea.st": "thedailybeast.com",
     "dmreg.co": "desmoinesregister.com",
     "bernie.to": "berniesanders.com",
@@ -169,15 +255,244 @@ LINK_SHORTENER_MAP = {
     "delonline.us": "delawareonline.com",
     "fb.me": "facebook.com",
     "azc.cc": "azcentral.com",
-    "ptrtvoic.es": "patriotvoices.com",
-    "cjky.it": "courier-journal.com",
-    "md.us": "state.md.us",
+    # "ptrtvoic.es": "patriotvoices.com",
+    # "cjky.it": "courier-journal.com",
+    # "md.us": "state.md.us",
     "goldenisles.news": "thebrunswicknews.com",
     "ohne.ws": "newarkadvocate.com",
     "thkpr.gs": "thinkprogress.org",
-    "hrld.us": "miamiherald.com",
-    "ma.us": "state.ma.us",
-    "ny.us": "state.ny.us",
-    "nm.us": "state.nm.us",
-    "huffpost.com": "huffingtonpost.com"
+    # "hrld.us": "miamiherald.com",
+    # "ma.us": "state.ma.us",
+    # "nyer.cm": "newyorker.com",
+    # "ny.us": "state.ny.us",
+    "wef.ch": "weforum.org",
+    "comsen.se": "commonsensemedia.org",
+    # "nm.us": "state.nm.us",
+    "huffpost.com": "huffingtonpost.com",
+    "huffp.st": "huffingtonpost.com",
+    "huff.to": "huffingtonpost.com",
+    "bbc.in": "bbc.co.uk",
+    "brook.gs": "brookings.edu",
+    "lp.ca": "lapresse.ca",
+    "virg.in": "virgin.com",
+    "hill.cm": "thehill.com",
+    "tnw.to": "thenextweb.com",
+    "dailym.ai": "dailymail.co.uk",
+    "instagr.am": "instagram.com",
+    "chng.it": "change.org",
+    "tws.io": "weeklystandard.com",
+    "spoti.fi": "spotify.com",
+    "nyp.st": "nypost.com",
+    "bv.ms": "bloomberg.com",
+    "glo.bo": "globo.com",
+    "gizmo.do": "gizmodo.com",
+    "prn.to": "prnewswire.com",
+    "usm.ag": "usmagazine.com",
+    "txnne.ws": "thetexan.news",
+    "u.pw": "upworthy.com",
+    # "uni.vi": "univision.com",
+    # "jwatch.us": "judicialwatch.org",
+    "ihr.fm": "iheart.com",
+    "hulu.tv": "hulu.com",
+    "herit.ag": "heritage.org",
+    # "fanda.co": "fandango.com",
+    "econ.st": "economist.com",
+    "dai.ly": "dailymotion.com",
+    "chn.ge": "change.org",
+    "ble.ac": "bleacherreport.com",
+    "aje.io": "aljazeera.com"
 }
+
+
+def canonical_link(url, timeout=5.0, session=None, user_agent=None):
+    '''
+    Try to resolve a link to the "most correct" version. Especially useful for expanding short URLs \
+    from bit.ly / Twitter and for checking HTTP status codes without retrieving the actual data.
+    :param url: The URL to test. Should be fully qualified.
+    :param timeout: How long to wait for a response before giving up (default is one second)
+    :param full_resolve: If set to True, each link will follow all redirects until its final endpoint.  By default, \
+    this is disabled (False) and redirects will only be followed if they are permanent (301 or 303 status codes)
+    :param session: A persistent session that can optionally be passed (useful if you're processing many links at once)
+    :return: The "canonical" URL as supplied by the server, or the original URL if the server was not helpful.
+    '''
+
+    # GOOD_STATUS_CODES = [200, 301, 303]
+    BAD_STATUS_CODES = [302, 307, 400, 404, 405, 407, 500, 501, 502, 503, 504, 520, 530, 404]
+    PROXY_REQUIRED = [307, 407]
+    CHECK_LENGTH = [301, 302, 200, 404]
+
+    close_session = False
+    if not session:
+        close_session = True
+        session = requests.Session()
+        session.headers.update({"User-Agent": user_agent})
+    if not url.startswith("http"):
+        url = "http://" + url
+    response = None
+    try:
+        response = session.head(url, allow_redirects=True, timeout=timeout)
+    except requests.ConnectionError:
+        try:
+            response = session.head(url, allow_redirects=False, timeout=timeout)
+        except:
+            pass
+    except:
+        pass
+
+    if response != None:
+
+        history = [(h.status_code, h.url) for h in response.history]
+        history.append((response.status_code, response.url))
+
+        last_good_url = history[0][1]
+        original_parsed = urlparse.urlparse(last_good_url)
+        has_path = original_parsed.path not in ["/", ""]
+        has_query = original_parsed.query != ""
+        last_good_status = None
+        for i, resp in enumerate(history):
+            status_code, response_url = resp
+            if "errors/404" in response_url:
+                # If it's clearly a 404 landing page, stop and use the last observed good URL
+                break
+            parsed = urlparse.urlparse(response_url)
+            if parsed.netloc in VANITY_LINK_SHORTENERS.keys() or parsed.netloc in GENERAL_LINK_SHORTENERS:
+                # Don't consider known shortened URLs
+                continue
+            if i != 0:
+                for param, val in urlparse.parse_qs(parsed.query).items():
+                    if len(val) == 1 and val[0].startswith("http"):
+                        parsed_possible_url = urlparse.urlparse(val[0])
+                        if parsed_possible_url.scheme and parsed_possible_url.netloc:
+                            # If the URL contains a GET parameter that is, itself, a URL, it's likely redirecting to it
+                            last_good_url = val[0]
+                            break
+            if status_code in PROXY_REQUIRED:
+                # These codes tend to indicate the last good URL in the chain
+                last_good_url = response_url
+                last_good_status = status_code
+                break
+            good_path = (not has_path or parsed.path not in ["/", ""])
+            good_query = (not has_query or parsed.query != "")
+            # If the URL has a path or some GET parameters, we'll inspect further
+            # Otherwise we just go with the previous URL
+            # Link shorteners are very rarely used to reference root domains
+            if good_query or good_path:
+                if (re.sub("https", "http", response_url) == re.sub("https", "http", last_good_url) or \
+                        parsed.path == original_parsed.path) or \
+                        response_url.lower() == last_good_url.lower():
+                    # If it's the same link but only the domain, protocol, or casing changed, it's fine
+                    last_good_url = response_url
+                    last_good_status = status_code
+                    # print("GOOD: {}, {}".format(status_code, response_url))
+                elif i != 0 and status_code in CHECK_LENGTH:
+                    # For these codes, we're going to see how much the link changed
+                    # Redirects and 404s sometimes preserve a decent URL, sometimes they go to a landing page
+                    # The following cutoffs seem to do a good job most of the time:
+                    # 1) The new URL has a long domain more than 7 characters, so it's not likely a shortened URL
+                    # 2) The prior URL had a long path and this one has fewer than 20 characters and it wasn't swapped out for GET params
+                    # 3) Or the prior URL had GET params and this one has far fewer and no replacement path
+                    # If these conditions are met, it's usually a generic 404 page
+                    bad = False
+                    if has_path and len(parsed.netloc) > 7 and len(parsed.path) < 20 and len(parsed.query) == 0:
+                        bad = True
+                    elif has_query and len(parsed.netloc) > 7 and len(parsed.query) < 20 and len(parsed.path) <= 1:
+                        bad = True
+                    if not bad:
+                        last_good_url = response_url
+                        last_good_status = status_code
+                        # print("GOOD: {}, {}".format(status_code, response_url))
+                    # else:
+                    #     # These can sometimes resolve further though, so we continue onward
+                    #     # print("SHORT URL, SKIPPING: {}".format(response_url))
+                    #     # break
+                else:
+                    # if status_code in GOOD_STATUS_CODES:
+                    #     print("GOOD: {}, {}".format(status_code, response_url))
+                    # elif status_code in BAD_STATUS_CODES:
+                    #     print("BAD: {}, {}".format(status_code, response_url))
+                    # else:
+                    #     print("UNKNOWN: {}, {}".format(status_code, response_url))
+                    if status_code not in BAD_STATUS_CODES:
+                        last_good_status = status_code
+                        last_good_url = response_url
+                    else:
+                        break
+            else:
+                # print("Resolved to a general URL, breaking off")
+                break
+
+        if status_code not in BAD_STATUS_CODES:
+            # If the URL ended on a good status code, we'll try to trim out any unnecessary GET parameters
+            # Often there's extra information about social sharing and referral sources that can be removed
+            ditch_params = []
+            parsed = urlparse.urlparse(last_good_url)
+            if parsed.query:
+                params = urlparse.parse_qs(parsed.query)
+                for k, v in params.items():
+                    # We iterate over all of the GET parameters and try holding each one out
+                    check = True
+                    for skipper in ["document", "article", "id", "qs"]:
+                        # If the parameter is named something that's probably a unique ID, we'll keep it
+                        if skipper in k.lower():
+                            check = False
+                    for skipper in ["html"]:
+                        # Same goes for parameters that contain URL information
+                        if skipper in v[0].lower():
+                            check = False
+                    if check:
+                        new_params = {k2: v2[0] for k2, v2 in params.items() if k2 != k and len(v2) == 1}
+                        new_params = urlparse.urlencode(new_params)
+                        new_parsed = parsed._replace(query=new_params)
+                        new_url = urlparse.urlunparse(new_parsed)
+                        resp = session.head(new_url, allow_redirects=True, timeout=timeout)
+                        new_parsed = urlparse.urlparse(resp.url)
+                        if new_parsed.query != "" or new_parsed.path not in ["", "/"]:
+                            # If removing a parameter didn't redirect to a root domain...
+                            new_url = resp.url
+                            compare_new = new_url.split("?")[0] if "?" in new_url else new_url
+                            compare_old = last_good_url.split("?")[0] if "?" in last_good_url else last_good_url
+                            if compare_new == compare_old:
+                                # And the domain is the same as it was before, then the parameter was probably unnecessary
+                                ditch_params.append(k)
+            if len(ditch_params) > 0:
+                # Now we remove all of the unnecessary get parameters and finalize the URL
+                new_params = {k: v[0] for k, v in params.items() if len(v) == 1 and k not in ditch_params}
+                new_params = urlparse.urlencode(new_params)
+                parsed = parsed._replace(query=new_params)
+                last_good_url = urlparse.urlunparse(parsed)
+
+        # if last_good_status in GOOD_STATUS_CODES:
+        #     print("FINAL GOOD: {}, {}".format(last_good_status, last_good_url))
+        # elif last_good_status in BAD_STATUS_CODES:
+        #     print("FINAL BAD: {}, {}".format(last_good_status, last_good_url))
+        # else:
+        #     print("FINAL UNKNOWN: {}, {}".format(last_good_status, last_good_url))
+
+        url = last_good_url
+
+    if close_session:
+        session.close()
+
+    return url
+
+
+def extract_domain_from_url(url, include_subdomain=True, resolve_url=False, timeout=1.0):
+    """
+    Attempts to extract a standardized domain from a url by following the link and extracting the TLD.
+    :param url:  The link from which to extract the domain
+    :param include_subdomain: Whether or not to include the subdomain (e.g. 'news.google.com'); default is True
+    :param resolve_url: Whether to fully resolve the URL.  If False (default), it will follow the URL but will not \
+    return the endpoint URL if it encounters a temporary redirect (i.e. redirects are only followed if they're permanent)
+    :return: The domain for the link
+    """
+
+    if resolve_url:
+        url = canonical_link(url, timeout=timeout)
+    domain = get_tld(url, fix_protocol=True, as_object=True, fail_silently=True)
+    if domain:
+        if include_subdomain and domain.subdomain:
+            domain = ".".join([domain.subdomain, domain.tld])
+        else:
+            domain = domain.tld
+        domain = VANITY_LINK_SHORTENERS.get(domain, domain)
+    return domain
--- tests/http.py
+++ tests/http.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-import unittest
+import unittest, re
 
 
 class HTTPTests(unittest.TestCase):
@@ -46,6 +46,64 @@ class HTTPTests(unittest.TestCase):
         with closing(open("tests/files/example_stripped_simple.html", "r")) as input:
             text = input.read()
             self.assertTrue(text == stripped_simple_html)
+
+    def test_canonical_link(self):
+
+        from pewtils.http import canonical_link
+        user_agent = "Mozilla/5.0 (compatible; MSIE 10.0; Macintosh; Intel Mac OS X 10_7_3; Trident/6.0)'"
+
+        # TODO: continue testing canonical_link
+        # canonical_link("https://t.co/H9tE3IlTAH")
+        # canonical_link("http://ow.ly/M5o4N")
+        # canonical_link("https://bit.ly/2CTCEFW")
+        # canonical_link("https://nbcnews.to/2Yc5JVz")
+        # canonical_link("https://www.c-span.org/Live-Video/C-SPAN")
+        # canonical_link("http://bit.ly/ascWO6")
+        # canonical_link("https://www.google.com/maps/d/viewer?mid=zQ8Zk-5ey-Y8.kgD9Rxu8JCNQ&hl=en&usp=sharing")
+        # canonical_link("https://goo.gl/images/WS5JSd")
+        for original_url, canonical_url in [
+            ("https://httpbin.org/redirect/10", "https://httpbin.org/relative-redirect/1"),
+            ("https://httpbin.org/redirect-to?url=status%2F200", ""),
+            ("https://httpbin.org/redirect-to?url=status%2F301", ""),
+            ("https://httpbin.org/redirect-to?url=status%2F302", ""),
+            ("https://httpbin.org/redirect-to?url=status%2F303", "https://httpbin.org/status/303"),
+            ("https://httpbin.org/redirect-to?url=status%2F307", "https://httpbin.org/status/307"),
+            ("https://httpbin.org/redirect-to?url=status%2F308", "https://httpbin.org/status/308"),
+            ("https://httpbin.org/redirect-to?url=status%2F400", ""),
+            ("https://httpbin.org/redirect-to?url=status%2F401", "https://httpbin.org/status/401"),
+            ("https://httpbin.org/redirect-to?url=status%2F403", "https://httpbin.org/status/403"),
+            ("https://httpbin.org/redirect-to?url=status%2F404", ""),
+            ("https://httpbin.org/redirect-to?url=status%2F500", ""),
+            ("https://httpbin.org/relative-redirect/10", ""),
+            ("https://pewrsr.ch/2kk3VvY", "https://www.pewinternet.org/2019/09/05/more-than-half-of-u-s-adults-trust-law-enforcement-to-use-facial-recognition-responsibly/"),
+            ("https://pewrsr.ch/2ly4LFE", "https://www.pewinternet.org/essay/the-challenges-of-using-machine-learning-to-identify-gender-in-images/"),
+            ("https://pewrsr.ch/2lxB0EX", "https://www.pewresearch.org/interactives/how-does-a-computer-see-gender/")
+        ]:
+            result = canonical_link(original_url, user_agent=user_agent, timeout=30)
+            # print(result)
+            # import pdb; pdb.set_trace()
+
+    def test_link_shortener_map(self):
+
+        import requests
+        from six.moves.urllib import parse as urlparse
+        from pewtils.http import VANITY_LINK_SHORTENERS
+        user_agent = "Mozilla/5.0 (compatible; MSIE 10.0; Macintosh; Intel Mac OS X 10_7_3; Trident/6.0)'"
+        session = requests.Session()
+        session.headers.update({"User-Agent": user_agent})
+        for k, v in VANITY_LINK_SHORTENERS.items():
+            resp = session.head("http://{}".format(k), allow_redirects=True, timeout=10)
+            resolved = re.sub("www\.", "", urlparse.urlparse(resp.url).netloc)
+            resolved = VANITY_LINK_SHORTENERS.get(resolved, resolved)
+            print("{} -> {}".format(k, resolved))
+            self.assertIn(resolved, ["bitly.com", v])
+        session.close()
+
+
+    def test_extract_domain_from_url(self):
+
+        pass
+
 
     def tearDown(self):
         pass
