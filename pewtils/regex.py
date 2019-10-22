57adf059e6f4f5d6d715e3afc6c3b9a9c1558ed0
pvankessel <pvankessel@pewresearch.org>
2019-10-22 11:42:41 -0400

    added unit tests for regular expressions and fixed a few issues with them

--- pewtils/regex.py
+++ pewtils/regex.py
@@ -2,16 +2,18 @@ import re
 
 
 URL_REGEX = re.compile(
-    r"((https?:\/\/(www\.)?)?[-a-zA-Z0-9@:%._\+~#=]{2,4096}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*))"
+    r"((?:https?:\/\/(?:www\.)?)?[-a-zA-Z0-9@:%._\+~#=]{2,4096}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&//=]*))"
 )
 
 DOMAIN_REGEX = re.compile(
     r"(?:http[s]?\:\/\/)?(?:www(?:s?)\.)?([\w\.\-]+)(?:[\\\/](?:.+))?"
 )
 
-HTTP_REGEX = re.compile(r"^http(s)?\:\/\/")
+HTTP_REGEX = re.compile(r"^http(?:s)?\:\/\/")
 
-US_DOLLAR_REGEX = re.compile(r"\$([0-9]{1,3}(?:(?:\,[0-9]{3})+)?(?:\.[0-9]{1,2})?)\s")
-TITLEWORD_REGEX = re.compile(r"\W([A-Z][a-z]+)\W")
-NUMBER_REGEX = re.compile(r"\W([0-9]+)\W")
+US_DOLLAR_REGEX = re.compile(
+    r"(\$(?:[1-9][0-9]{0,2}(?:(?:\,[0-9]{3})+)?(?:\.[0-9]{1,2})?))\b"
+)
+TITLEWORD_REGEX = re.compile(r"\b([A-Z][a-z]+)\b")
+NUMBER_REGEX = re.compile(r"\b([0-9]+)\b")
 NONALPHA_REGEX = re.compile(r"[^\w]")
