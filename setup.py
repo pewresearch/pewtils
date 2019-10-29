dd90e017defebb7174104b74e3e52797199d2ec4
pvankessel <pvankessel@pewresearch.org>
2019-10-29 10:21:31 -0400

    Repo is now beta quality

--- setup.py
+++ setup.py
@@ -28,10 +28,7 @@ setup(
     include_package_data=True,
     classifiers=[
         # https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        #        'Development Status :: 1 - Planning',
-        #        'Development Status :: 2 - Pre-Alpha',
-        "Development Status :: 3 - Alpha",
-        #        'Development Status :: 4 - Beta',
+        "Development Status :: 4 - Beta",
         #        'Development Status :: 5 - Production/Stable',
         #        'Development Status :: 6 - Mature',
         #        'Development Status :: 7 - Inactive'
