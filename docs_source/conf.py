97503dd93492a9b68bde8577e06468f147775234
pvankessel <pvankessel@pewresearch.org>
2020-05-20 12:21:59 -0400

    blackify

--- docs_source/conf.py
+++ docs_source/conf.py
@@ -46,7 +46,7 @@ extensions = [
     "sphinx.ext.ifconfig",
     "sphinx.ext.viewcode",
     "sphinx.ext.githubpages",
-    "autodocsumm"
+    "autodocsumm",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
