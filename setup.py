1953ba1ecd12dad51e7da503dfb96b7ab569580f
Brian Broderick <brian@broderick.io>
2022-02-02 00:49:41 +0000

    Bump version: 1.1.5.dev1 → 1.1.5.dev2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt") as reqs:
 
 setup(
     name="pewtils",
-    version="1.1.5.dev1",
+    version="1.1.5.dev2",
     description="General programming utilities from Pew Research Center",
     long_description=readme,
     long_description_content_type="text/markdown",
